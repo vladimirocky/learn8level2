edb76ceb067135d345a9bea0d86ba41daed2cf43
Timur Markovnikov <gadzhibekovtimur@gmail.com>
2022-06-05 18:04:19 +0300

    Выполнено...

--- Моё задание/asses/js/AnimatioScript.js
+++ Моё задание/asses/js/AnimatioScript.js
@@ -1,34 +1,3 @@
-let offset = 0;
-const textSlider = document.getElementById('TextImg');
-const sliderLine= document.querySelector('.slider-line');
-const counter = document.getElementById('counterSlider');
-
-const btnNext = document.querySelector('.slider-next');
-btnNext.addEventListener('click', () => {
-    offset = offset + 800;
-    if (offset > 1600) offset = 0;
-    sliderLine.style.left = -offset + 'px';
-
-    switch (offset) {
-        case 0: textSlider.innerHTML = "Горы Тарки"; counter.innerHTML = "1/3"; break;
-        case 800: textSlider.innerHTML = "Гора Базардюзю!"; counter.innerHTML = "2/3"; break;
-        case 1600: textSlider.innerHTML = "Вокзал Махачкала"; counter.innerHTML = "3/3"; break;
-    }
-})
-
-const btnPrev = document.querySelector('.slider-prev');
-btnPrev.addEventListener('click', () => {
-    offset = offset - 800;
-    if (offset < 0) offset = 1600;
-    sliderLine.style.left = -offset + 'px';
-    
-    switch (offset) {
-        case 0: textSlider.innerHTML = "Горы Тарки"; counter.innerHTML = "1/3"; break;
-        case 800: textSlider.innerHTML = "Гора Базардюзю!"; counter.innerHTML = "2/3"; break;
-        case 1600: textSlider.innerHTML = "Вокзал Махачкала"; counter.innerHTML = "3/3"; break;
-    }
-})
-
 let listOfComments;
 let addUser = document.getElementById("userName");
 let textComment = document.getElementById("text_comment");
